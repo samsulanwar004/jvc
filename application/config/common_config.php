7654ad383cab28daad5f0442b875660020ecd5d1
Samsul Anwar <samsul@indomog.com>
2016-06-30 16:13:04 +0700

    login with facebook

--- application/config/common_config.php
+++ application/config/common_config.php
@@ -4,3 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // Config Email
 $config['user_email'] = "Jogjakarta V-ixion Community";
 $config['email'] = "admin@jvc.or.id";
+
+// Config FB
+
+$config['facebook']['api_id']       = '641511549358745';
+$config['facebook']['app_secret']   = '9dbd383e1acdd1fc8d437f1308cc632b';
+$config['facebook']['redirect_url'] = 'http://localhost/jvc/login/masuk';
+$config['facebook']['permissions']  = array(
+                                        'email',
+                                        'public_profile'
+                                      );
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+require_once( './vendor/facebook/autoload.php' );
 class Login extends CI_Controller {
 
 	public function __construct()
@@ -81,5 +81,112 @@ class Login extends CI_Controller {
 		$this->load->view('templates/home/header', $data);
 		$this->load->view('view_notif');
 		$this->load->view('templates/home/footer');
-}
+	}
+
+	public function login_facebook()
+	{
+
+		$fb = new Facebook\Facebook([
+		  'app_id' => $this->config->item('facebook')['api_id'],
+		  'app_secret' => $this->config->item('facebook')['app_secret'],
+		  'default_graph_version' => 'v2.5',
+		]);
+		$helper = $fb->getRedirectLoginHelper();
+
+		$permissions = $this->config->item('facebook')['permissions']; // Optional permissions
+		$loginUrl = $helper->getLoginUrl($this->config->item('facebook')['redirect_url'], $permissions);
+
+		echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+		//$this->load->view('view_facebook');
+	}
+
+	public function masuk()
+	{
+		$fb = new Facebook\Facebook([
+		  'app_id' => $this->config->item('facebook')['api_id'],
+		  'app_secret' => $this->config->item('facebook')['app_secret'],
+		  'default_graph_version' => 'v2.5',
+		]);
+
+		$helper = $fb->getRedirectLoginHelper();
+
+		try {
+		  $accessToken = $helper->getAccessToken();
+		} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		  // When Graph returns an error
+		  echo 'Graph returned an error: ' . $e->getMessage();
+		  exit;
+		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		  // When validation fails or other local issues
+		  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		  exit;
+		}
+
+		if (! isset($accessToken)) {
+		  if ($helper->getError()) {
+		    header('HTTP/1.0 401 Unauthorized');
+		    echo "Error: " . $helper->getError() . "\n";
+		    echo "Error Code: " . $helper->getErrorCode() . "\n";
+		    echo "Error Reason: " . $helper->getErrorReason() . "\n";
+		    echo "Error Description: " . $helper->getErrorDescription() . "\n";
+		  } else {
+		    header('HTTP/1.0 400 Bad Request');
+		    echo 'Bad request';
+		  }
+		  exit;
+		}
+
+		// Logged in
+		echo '<h3>Access Token</h3>';
+		var_dump($accessToken->getValue());
+
+		// The OAuth 2.0 client handler helps us manage access tokens
+		$oAuth2Client = $fb->getOAuth2Client();
+
+		// Get the access token metadata from /debug_token
+		$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+		echo '<h3>Metadata</h3>';
+		var_dump($tokenMetadata);
+
+		// Validation (these will throw FacebookSDKException's when they fail)
+		$tokenMetadata->validateAppId($this->config->item('facebook')['api_id']); // Replace {app-id} with your app id
+		// If you know the user ID this access token belongs to, you can validate it here
+		//$tokenMetadata->validateUserId('123');
+		$tokenMetadata->validateExpiration();
+
+		if (! $accessToken->isLongLived()) {
+		  // Exchanges a short-lived access token for a long-lived one
+		  try {
+		    $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+		  } catch (Facebook\Exceptions\FacebookSDKException $e) {
+		    echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+		    exit;
+		  }
+
+		  echo '<h3>Long-lived</h3>';
+		  var_dump($accessToken->getValue());
+		}
+
+		$_SESSION['fb_access_token'] = (string) $accessToken;
+
+		try {
+			  // Returns a `Facebook\FacebookResponse` object
+			  $response = $fb->get('/me?fields=id,name', $accessToken);
+			} catch(Facebook\Exceptions\FacebookResponseException $e) {
+			  echo 'Graph returned an error: ' . $e->getMessage();
+			  exit;
+			} catch(Facebook\Exceptions\FacebookSDKException $e) {
+			  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+			  exit;
+			}
+
+			$user = $response->getGraphUser();
+
+			echo 'Name: ' . $user['name'];
+			// OR
+			// echo 'Name: ' . $user->getName();
+			// User is logged in with a long-lived access token.
+			// You can redirect them to a members-only page.
+			//header('Location: https://example.com/members.php');
+	}
 }
